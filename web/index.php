c9f7099542930299b37ec792aea78b87600c4598
Rodrigo Manozzo <rodrigo.manozzo@sistemafiep.org.br>
2022-06-27 00:18:38 -0300

    update env project

--- web/index.php
+++ web/index.php
@@ -2,12 +2,12 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-try {
-    $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
-    $dotenv->load();
-} catch (\Throwable $th) {
-    throw $th;
-}
+// try {
+//     $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
+//     $dotenv->load();
+// } catch (\Throwable $th) {
+//     throw $th;
+// }
 
 // defined('YII_DEBUG') || define('YII_DEBUG', (getenv('YII_DEBUG') === 'true'));
 // defined('YII_ENV') ?: define('YII_ENV', getenv('YII_ENV'));
